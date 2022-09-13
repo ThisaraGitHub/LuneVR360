8ed7996a0eba51f8ce91adf96e9e81c0cdf3ac42
Thisara Sandaruwan <30999158+ThisaraGitHub@users.noreply.github.com>
2022-09-13 15:33:02 +0530

    content added

--- LuneVR360/Assets/Scripts/ContentManagers/ContentManager.cs
+++ LuneVR360/Assets/Scripts/ContentManagers/ContentManager.cs
@@ -10,7 +10,7 @@ public class ContentManager : MonoBehaviour
     public int panelNumber;
     public LocalizedText tittleText;
     public LocalizedText bodyText;
-    //public LocalizedText selectionText;
+    public LocalizedText selectionText;
     // Start is called before the first frame update
     void Start()
     {
@@ -33,7 +33,26 @@ public class ContentManager : MonoBehaviour
                     case 0:
                         tittleText.LocalizationKey = "Panel5.Image1Tittle";
                         bodyText.LocalizationKey = "Panel5.Image1ContentDescription";
-                        //selectionText.LocalizationKey = "Panel5.Image1SelectionText";
+                        selectionText.LocalizationKey = "Panel5.Image1SelectionText";
+                        tittleText.Localize();
+                        bodyText.Localize();
+                        selectionText.Localize();
+                        break;   
+                    case 1:
+                        tittleText.LocalizationKey = "Panel5.Image2Tittle";
+                        bodyText.LocalizationKey = "Panel5.Image2ContentDescription";
+                        selectionText.LocalizationKey = "Panel5.Image2SelectionText";
+                        tittleText.Localize();
+                        bodyText.Localize();
+                        selectionText.Localize();
+                        break;
+                    case 2:
+                        tittleText.LocalizationKey = "Panel5.Image3Tittle";
+                        bodyText.LocalizationKey = "Panel5.Image3ContentDescription";
+                        selectionText.LocalizationKey = "Panel5.Image3SelectionText";
+                        tittleText.Localize();
+                        bodyText.Localize();
+                        selectionText.Localize();
                         break;
 
                 }
