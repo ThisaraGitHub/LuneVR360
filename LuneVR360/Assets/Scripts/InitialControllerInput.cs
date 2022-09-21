0476356a359d697406043163aa56f4fbd77f38e4
Thisara Sandaruwan <30999158+ThisaraGitHub@users.noreply.github.com>
2022-09-21 13:32:44 +0530

    toggle ui added

--- LuneVR360/Assets/Scripts/InitialControllerInput.cs
+++ LuneVR360/Assets/Scripts/InitialControllerInput.cs
@@ -8,51 +8,44 @@ namespace Lune.VR360
 {
     public class InitialControllerInput : MonoBehaviour
     {
+        public GameObject canvasObject;
         public ScreenManager screenManager;
         public VRImageSelection vRImageSelection;
-        private bool onetime = false;
+        public bool onetime = false;
+        public bool isAbuttonClicked = false;
+        public float time;
+        public float delayedTime;
+        public bool isIdleTimeReached = false;
+
+        public bool modGodspeed = false;
         // Start is called before the first frame update
         void Start()
         {
             screenManager.ControllerUI(true);
+            time = 0;
+            delayedTime = 3;
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (!screenManager.isPanel2Enabled &&
-                !screenManager.isPanel3Enabled &&
-                !screenManager.isPanel4Enabled &&
-                !screenManager.isPanel5Enabled &&
-                !screenManager.isPanel6Enabled &&
-                !screenManager.isPanel7Enabled)
+            if (OVRInput.GetDown(OVRInput.Button.One))
             {
-                if (OVRInput.Get(OVRInput.Button.One))
+                if (this.modGodspeed)
                 {
-                    if (!onetime)
-                    {
-                        Debug.Log("A button pressed");
-                        screenManager.ControllerUI(false);
-                        screenManager.TittleScreenUI(true);
-                        onetime = true;
-                    }
+                    //this.speedMult = 1f;
+                    print("Off");
+                    screenManager.ControllerUI(true);
+                    this.modGodspeed = false;
                 }
-
-               
+                else if (!this.modGodspeed)
+                {
+                    //this.speedMult = 5f;
+                    print("On");
+                    screenManager.ControllerUI(false);
+                    this.modGodspeed = true;
+                }
             }
-
-            if (onetime &&
-                   screenManager.isPanel2OpnedOnce &&
-                   screenManager.isPanel3OpnedOnce &&
-                   screenManager.isPanel4OpnedOnce &&
-                   screenManager.isPanel5OpnedOnce &&
-                   screenManager.isPanel6OpnedOnce &&
-                   screenManager.isPanel7OpnedOnce &&
-                   OVRInput.Get(OVRInput.Button.One))
-            {
-                vRImageSelection.Screen7ActiveStatus(true);
-            }
-
         }
     }
 }
